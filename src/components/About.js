8d48baa6508862e80b7a7d864f7a28390d1c5b44
kayalvizhi <madhukayal88@gmail.com>
2026-07-30 21:52:38 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,22 @@
+import React from 'react';
 
+function About() {
+  return (
+    <div className="page-container">
+      <h2>📖 About Page</h2>
+      <p className="about-message">This is a sample React Router program.</p>
+      <div className="page-content">
+        <p>This application demonstrates how to use React Router for navigation.</p>
+        <p>Features:</p>
+        <ul>
+          <li>BrowserRouter for routing</li>
+          <li>Routes and Route for page rendering</li>
+          <li>Link components for navigation</li>
+          <li>Dynamic URL updates</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default About;
