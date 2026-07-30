97183eda4c8a31b4f86028c1f749e707f3f355ca
kayalvizhi <madhukayal88@gmail.com>
2026-07-30 21:52:19 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+function Home() {
+  return (
+    <div className="page-container">
+      <h2>🏠 Home Page</h2>
+      <p className="welcome-message">Welcome to my website!</p>
+      <div className="page-content">
+        <p>This is the home page of our React Router application.</p>
+        <p>Use the navigation links above to switch between pages.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
