8b4e83802b1f1c7ec4c2adff7e752fe7cd68df95
kayalvizhi <madhukayal88@gmail.com>
2026-07-30 21:52:55 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Navigation() {
+  return (
+    <nav className="nav-container">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/about" className="nav-link">About</Link>
+    </nav>
+  );
+}
+
+export default Navigation;
